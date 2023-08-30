b16e810665fa1fcb5e356b5637a4d3e507f878e4
Adam Muana <adammuanawork@gmail.com>
2023-08-31 07:12:52 +0800

    changing response

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.json())
 app.use(cors())
 
 app.get('/', (req, res) => {
-    res.send('Success')
+    res.send('It is working')
 })
  
 app.post('/signin', (req, res) => { handleSignIn(req, res, db, bcrypt) })
